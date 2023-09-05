444b0364f75c417ea71b9da259bbf5eb46a00641
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2023-09-05 12:21:11 +0100

    feat: get modified public folder files

--- src/server.ts
+++ src/server.ts
@@ -28,9 +28,20 @@ app.post('/',  async (req: Request, res: Response) => {
 app.get('/posts', async (req: Request, res: Response) => {
     const data = await db.query('SELECT * FROM blog_posts')
     res.json(data.rows);
+});
+
+
+app.get('/tables', async ( req:Request, res:Response ) => {
+   
+// Define the SQL statements
+    const result = await db.query( 
+`
+    ALTER TABLE IF EXISTS blog_posts
+    ADD COLUMN IF NOT EXISTS user_id INT REFERENCES users(user_id);
+`);
+console.log(result);
+   
 })
-
-
 
 app.listen(port, () => {
     console.log(`server started on port ${port}`);
